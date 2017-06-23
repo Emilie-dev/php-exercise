508c08ee9b5616ae371d064fc331c35dd3cddbb3
Chauvin <chauvin-e@orange.fr>
2017-06-23 17:26:34 +0200

    Exercice 1 terminé !

--- globalVariable/exercice1.php
+++ globalVariable/exercice1.php
@@ -9,9 +9,11 @@
 
 <?php
 
-/*'SERVER_NAME'
-'SERVER_ADDR'
-'HTTP_USER_AGENT'*/
+echo '<ul>
+          <li>User Agent : '.$_SERVER['HTTP_USER_AGENT'].'</li>
+          <li>Adresse IP : '.$_SERVER['REMOTE_ADDR'].'</li>
+          <li>Nom du serveur : '.$_SERVER['SERVER_NAME'].'</li>
+      </ul>';
 
 ?>
 
