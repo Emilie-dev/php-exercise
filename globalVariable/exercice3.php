07f83fcfc2b2c389d08c695ad660747f7b4925b2
Chauvin <chauvin-e@orange.fr>
2017-06-26 11:17:40 +0200

    add exercice 3 et 4

---
+++ globalVariable/exercice3.php
@@ -0,0 +1,25 @@
+<?php
+
+setcookie('login', 'toto', time() + 365*24*3600, null, null, false, true);
+setcookie('password', '1234', time() + 365*24*3600, null, null, false, true);
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Exercice 3 - GlobalVariable</title>
+</head>
+<body>
+
+<h2>Exercice sur les cookies :</h2>
+
+<form method="post" action="exercice4.php">
+	<input type="text" name="login" placeholder="login">
+	<input type="password" name="password">
+	<input type="submit" name="submit" value="OK">
+</form>
+
+
+</body>
+</html>
---
+++ globalVariable/exercice4.php
@@ -0,0 +1,6 @@
+<?php
+
+echo 'Votre login est : '.$_COOKIE['login'].' </br>';
+echo 'Votre mot de passe est : '.$_COOKIE['password'];
+
+?>
